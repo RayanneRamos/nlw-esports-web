4d54a7eb8f8714bbd68be27a6e45a80c70aae804
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-09-16 11:41:52 -0300

    Criando a chamada pra API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
+import { useState, useEffect } from 'react';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+  const [ games, setGames ] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt='Logo' />
@@ -11,11 +31,16 @@ function App() {
         Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.
       </h1>
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner  
-          bannerUrl='/game-1.png'
-          title='League of Legends'
-          adsCount={5}
-        />  
+        { games.map(game => {
+          return (
+            <GameBanner 
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
       <CreateAdBanner />
       
